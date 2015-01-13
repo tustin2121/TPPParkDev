e74378ee12a1ae81892cdd8270e309b2831652ea
Tim Pittman <tustin2121@gmail.com>
2015-01-13 18:17:56 -0500

    More that token mapwork
    - Getting into some of these locations... :P

--- _work-files/Maps/WarpPointTileData.tsx
+++ _work-files/Maps/WarpPointTileData.tsx
@@ -64,37 +64,37 @@
  <tile id="10">
   <properties>
    <property name="entryanim" value="0"/>
-   <property name="warppoint" value="A"/>
+   <property name="warppoint" value="10"/>
   </properties>
  </tile>
  <tile id="11">
   <properties>
    <property name="entryanim" value="0"/>
-   <property name="warppoint" value="B"/>
+   <property name="warppoint" value="11"/>
   </properties>
  </tile>
  <tile id="12">
   <properties>
    <property name="entryanim" value="0"/>
-   <property name="warppoint" value="C"/>
+   <property name="warppoint" value="12"/>
   </properties>
  </tile>
  <tile id="13">
   <properties>
    <property name="entryanim" value="0"/>
-   <property name="warppoint" value="D"/>
+   <property name="warppoint" value="13"/>
   </properties>
  </tile>
  <tile id="14">
   <properties>
    <property name="entryanim" value="0"/>
-   <property name="warppoint" value="E"/>
+   <property name="warppoint" value="14"/>
   </properties>
  </tile>
  <tile id="15">
   <properties>
    <property name="entryanim" value="0"/>
-   <property name="warppoint" value="F"/>
+   <property name="warppoint" value="15"/>
   </properties>
  </tile>
  <tile id="16">
@@ -160,37 +160,37 @@
  <tile id="26">
   <properties>
    <property name="entryanim" value="1"/>
-   <property name="warppoint" value="A"/>
+   <property name="warppoint" value="10"/>
   </properties>
  </tile>
  <tile id="27">
   <properties>
    <property name="entryanim" value="1"/>
-   <property name="warppoint" value="B"/>
+   <property name="warppoint" value="11"/>
   </properties>
  </tile>
  <tile id="28">
   <properties>
    <property name="entryanim" value="1"/>
-   <property name="warppoint" value="C"/>
+   <property name="warppoint" value="12"/>
   </properties>
  </tile>
  <tile id="29">
   <properties>
    <property name="entryanim" value="1"/>
-   <property name="warppoint" value="D"/>
+   <property name="warppoint" value="13"/>
   </properties>
  </tile>
  <tile id="30">
   <properties>
    <property name="entryanim" value="1"/>
-   <property name="warppoint" value="E"/>
+   <property name="warppoint" value="14"/>
   </properties>
  </tile>
  <tile id="31">
   <properties>
    <property name="entryanim" value="1"/>
-   <property name="warppoint" value="F"/>
+   <property name="warppoint" value="15"/>
   </properties>
  </tile>
  <tile id="32">
@@ -256,37 +256,37 @@
  <tile id="42">
   <properties>
    <property name="entryanim" value="2"/>
-   <property name="warppoint" value="A"/>
+   <property name="warppoint" value="10"/>
   </properties>
  </tile>
  <tile id="43">
   <properties>
    <property name="entryanim" value="2"/>
-   <property name="warppoint" value="B"/>
+   <property name="warppoint" value="11"/>
   </properties>
  </tile>
  <tile id="44">
   <properties>
    <property name="entryanim" value="2"/>
-   <property name="warppoint" value="C"/>
+   <property name="warppoint" value="12"/>
   </properties>
  </tile>
  <tile id="45">
   <properties>
    <property name="entryanim" value="2"/>
-   <property name="warppoint" value="D"/>
+   <property name="warppoint" value="13"/>
   </properties>
  </tile>
  <tile id="46">
   <properties>
    <property name="entryanim" value="2"/>
-   <property name="warppoint" value="E"/>
+   <property name="warppoint" value="14"/>
   </properties>
  </tile>
  <tile id="47">
   <properties>
    <property name="entryanim" value="2"/>
-   <property name="warppoint" value="F"/>
+   <property name="warppoint" value="15"/>
   </properties>
  </tile>
  <tile id="48">
@@ -352,37 +352,37 @@
  <tile id="58">
   <properties>
    <property name="entryanim" value="3"/>
-   <property name="warppoint" value="A"/>
+   <property name="warppoint" value="10"/>
   </properties>
  </tile>
  <tile id="59">
   <properties>
    <property name="entryanim" value="3"/>
-   <property name="warppoint" value="B"/>
+   <property name="warppoint" value="11"/>
   </properties>
  </tile>
  <tile id="60">
   <properties>
    <property name="entryanim" value="3"/>
-   <property name="warppoint" value="C"/>
+   <property name="warppoint" value="12"/>
   </properties>
  </tile>
  <tile id="61">
   <properties>
    <property name="entryanim" value="3"/>
-   <property name="warppoint" value="D"/>
+   <property name="warppoint" value="13"/>
   </properties>
  </tile>
  <tile id="62">
   <properties>
    <property name="entryanim" value="3"/>
-   <property name="warppoint" value="E"/>
+   <property name="warppoint" value="14"/>
   </properties>
  </tile>
  <tile id="63">
   <properties>
    <property name="entryanim" value="3"/>
-   <property name="warppoint" value="F"/>
+   <property name="warppoint" value="15"/>
   </properties>
  </tile>
  <tile id="64">
@@ -448,37 +448,37 @@
  <tile id="74">
   <properties>
    <property name="entryanim" value="4"/>
-   <property name="warppoint" value="A"/>
+   <property name="warppoint" value="10"/>
   </properties>
  </tile>
  <tile id="75">
   <properties>
    <property name="entryanim" value="4"/>
-   <property name="warppoint" value="B"/>
+   <property name="warppoint" value="11"/>
   </properties>
  </tile>
  <tile id="76">
   <properties>
    <property name="entryanim" value="4"/>
-   <property name="warppoint" value="C"/>
+   <property name="warppoint" value="12"/>
   </properties>
  </tile>
  <tile id="77">
   <properties>
    <property name="entryanim" value="4"/>
-   <property name="warppoint" value="D"/>
+   <property name="warppoint" value="13"/>
   </properties>
  </tile>
  <tile id="78">
   <properties>
    <property name="entryanim" value="4"/>
-   <property name="warppoint" value="E"/>
+   <property name="warppoint" value="14"/>
   </properties>
  </tile>
  <tile id="79">
   <properties>
    <property name="entryanim" value="4"/>
-   <property name="warppoint" value="F"/>
+   <property name="warppoint" value="15"/>
   </properties>
  </tile>
  <tile id="80">
@@ -544,37 +544,37 @@
  <tile id="90">
   <properties>
    <property name="entryanim" value="5"/>
-   <property name="warppoint" value="A"/>
+   <property name="warppoint" value="10"/>
   </properties>
  </tile>
  <tile id="91">
   <properties>
    <property name="entryanim" value="5"/>
-   <property name="warppoint" value="B"/>
+   <property name="warppoint" value="11"/>
   </properties>
  </tile>
  <tile id="92">
   <properties>
    <property name="entryanim" value="5"/>
-   <property name="warppoint" value="C"/>
+   <property name="warppoint" value="12"/>
   </properties>
  </tile>
  <tile id="93">
   <properties>
    <property name="entryanim" value="5"/>
-   <property name="warppoint" value="D"/>
+   <property name="warppoint" value="13"/>
   </properties>
  </tile>
  <tile id="94">
   <properties>
    <property name="entryanim" value="5"/>
-   <property name="warppoint" value="E"/>
+   <property name="warppoint" value="14"/>
   </properties>
  </tile>
  <tile id="95">
   <properties>
    <property name="entryanim" value="5"/>
-   <property name="warppoint" value="F"/>
+   <property name="warppoint" value="15"/>
   </properties>
  </tile>
  <tile id="96">
@@ -640,133 +640,117 @@
  <tile id="106">
   <properties>
    <property name="entryanim" value="6"/>
-   <property name="warppoint" value="A"/>
+   <property name="warppoint" value="10"/>
   </properties>
  </tile>
  <tile id="107">
   <properties>
    <property name="entryanim" value="6"/>
-   <property name="warppoint" value="B"/>
+   <property name="warppoint" value="11"/>
   </properties>
  </tile>
  <tile id="108">
   <properties>
    <property name="entryanim" value="6"/>
-   <property name="warppoint" value="C"/>
+   <property name="warppoint" value="12"/>
   </properties>
  </tile>
  <tile id="109">
   <properties>
    <property name="entryanim" value="6"/>
-   <property name="warppoint" value="D"/>
+   <property name="warppoint" value="13"/>
   </properties>
  </tile>
  <tile id="110">
   <properties>
    <property name="entryanim" value="6"/>
-   <property name="warppoint" value="E"/>
+   <property name="warppoint" value="14"/>
   </properties>
  </tile>
  <tile id="111">
   <properties>
    <property name="entryanim" value="6"/>
-   <property name="warppoint" value="F"/>
+   <property name="warppoint" value="15"/>
   </properties>
  </tile>
  <tile id="112">
   <properties>
-   <property name="entryanim" value="7"/>
-   <property name="warppoint" value="0"/>
+   <property name="warpbank" value="0"/>
   </properties>
  </tile>
  <tile id="113">
   <properties>
-   <property name="entryanim" value="7"/>
-   <property name="warppoint" value="1"/>
+   <property name="warpbank" value="1"/>
   </properties>
  </tile>
  <tile id="114">
   <properties>
-   <property name="entryanim" value="7"/>
-   <property name="warppoint" value="2"/>
+   <property name="warpbank" value="2"/>
   </properties>
  </tile>
  <tile id="115">
   <properties>
-   <property name="entryanim" value="7"/>
-   <property name="warppoint" value="3"/>
+   <property name="warpbank" value="3"/>
   </properties>
  </tile>
  <tile id="116">
   <properties>
-   <property name="entryanim" value="7"/>
-   <property name="warppoint" value="4"/>
+   <property name="warpbank" value="4"/>
   </properties>
  </tile>
  <tile id="117">
   <properties>
-   <property name="entryanim" value="7"/>
-   <property name="warppoint" value="5"/>
+   <property name="warpbank" value="5"/>
   </properties>
  </tile>
  <tile id="118">
   <properties>
-   <property name="entryanim" value="7"/>
-   <property name="warppoint" value="6"/>
+   <property name="warpbank" value="6"/>
   </properties>
  </tile>
  <tile id="119">
   <properties>
-   <property name="entryanim" value="7"/>
-   <property name="warppoint" value="7"/>
+   <property name="warpbank" value="7"/>
   </properties>
  </tile>
  <tile id="120">
   <properties>
-   <property name="entryanim" value="7"/>
-   <property name="warppoint" value="8"/>
+   <property name="warpbank" value="8"/>
   </properties>
  </tile>
  <tile id="121">
   <properties>
-   <property name="entryanim" value="7"/>
-   <property name="warppoint" value="9"/>
+   <property name="warpbank" value="9"/>
   </properties>
  </tile>
  <tile id="122">
   <properties>
-   <property name="entryanim" value="7"/>
-   <property name="warppoint" value="A"/>
+   <property name="warpbank" value="10"/>
   </properties>
  </tile>
  <tile id="123">
   <properties>
-   <property name="entryanim" value="7"/>
-   <property name="warppoint" value="B"/>
+   <property name="warpbank" value="11"/>
   </properties>
  </tile>
  <tile id="124">
   <properties>
-   <property name="entryanim" value="7"/>
-   <property name="warppoint" value="C"/>
+   <property name="warpbank" value="12"/>
   </properties>
  </tile>
  <tile id="125">
   <properties>
-   <property name="entryanim" value="7"/>
-   <property name="warppoint" value="D"/>
+   <property name="warpbank" value="13"/>
   </properties>
  </tile>
  <tile id="126">
   <properties>
-   <property name="entryanim" value="7"/>
-   <property name="warppoint" value="E"/>
+   <property name="warpbank" value="14"/>
   </properties>
  </tile>
  <tile id="127">
   <properties>
-   <property name="entryanim" value="7"/>
-   <property name="warppoint" value="F"/>
+   <property name="warpbank" value="15"/>
   </properties>
  </tile>
 </tileset>
