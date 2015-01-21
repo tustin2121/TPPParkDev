58c7f93c587ed609edf4aa227d7ba3efa91d2055
Tim Pittman <tustin2121@gmail.com>
2015-01-21 17:34:08 -0500

    Event images!

---
+++ src/events/meta.community/EnderShaman.viewer/base.js
@@ -0,0 +1,14 @@
+// EnderShaman.viewer/base.js
+// Defines the base event for myself in the park
+
+//$ PackConfig
+{ "sprites" : [ "base.png" ] }
+//$!
+module.exports = {
+	id: "EnderShaman.viewer",
+	sprite: "base.png",
+	sprite_format: "hg_vertmix-32",
+	
+	name: "Level44EnderShaman",
+	infodex: "meta.community.endershaman",
+};
---
+++ src/events/s1.red/Red.trainer/base.js
@@ -0,0 +1,15 @@
+// Red.trainer/base.js
+// Defines the base event for Red
+
+//$ PackConfig
+{ "sprites" : [ "base.png" ] }
+//$!
+
+module.exports = {
+	id: "Red.trainer",
+	sprite: "base.png",
+	sprite_format: "pt_vertcol-32",
+	
+	name: "Red",
+	infodex: "game.red.trainer.red",
+};
