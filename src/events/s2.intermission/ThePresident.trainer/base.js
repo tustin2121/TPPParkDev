acce7decd4e108589b3982317e40fbe90d43e71b
Tustin2121 <tustin2121@gmail.com>
2015-02-10 00:51:12 -0500

    Token work
    - Surldab Map tweak
    - Propagating that change to loading to other html files
    - Distracted by the fact that the stream has started intermission games,
    earlier than expected.

---
+++ src/events/s2.intermission/ThePresident.trainer/base.js
@@ -0,0 +1,33 @@
+// ThePresident.trainer/base.js
+// 
+
+//$ PackConfig
+{ "sprites" : [ "base.png" ] }
+//$!
+module.exports = {
+	id: "ThePresident.trainer",
+	sprite: "base.png",
+	sprite_format: "hg_pokecol-32",
+	
+	name: "The President",
+	infodex: "game.intermission.robopon.cody",
+	
+	// sprite_creator: "Carlotta4th",
+};
+
+/* 
+http://www.reddit.com/r/twitchplayspokemon/comments/2vdbbv/art_the_president/
+
+	In the game the protagonist's grandpa is the president of the Robopon company, 
+	and he's retiring and setting you up as the new company president, and he gives 
+	you his first robot (Sunny, in the picture) to start you off.
+	
+	But we kept using "a9" to speed through the dialogue, so chat heard something about 
+	how we were the new president and we were all hyped but had no idea why and what we 
+	were the new president of until we reset the game and had to read that dialogue a 
+	couple more times. :p
+
+The main character of RoboPon Sun, Cody. Though we gave him many other four-letter names,
+the most famous being "Cunt", and "Anal".
+
+*/
