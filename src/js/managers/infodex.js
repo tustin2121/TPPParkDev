3e788345de0f116446f3cc4f3e4baa3aeb0ee50b
Tim Pittman <tustin2121@gmail.com>
2015-03-19 19:40:49 -0400

    More token work on the Infodex

--- src/js/managers/infodex.js
+++ src/js/managers/infodex.js
@@ -6,10 +6,32 @@ var inherits = require("inherits");
 var extend = require("extend");
 
 function Infodex() {
+	var self = this;
+	$(function(){
+		self.breadcrumb = $("#dex-container .breadcrumb");
+		self.searchbox = $("#dex-container .search input");
+		
+		$("#dex-container .backbtn").click(function(){
+			
+		});
+		$("#dex-container .fwdbtn").click(function(){
+			
+		});
+		$("#dex-container .searchbtn").click(function(){
+			
+		});
+	});
 	
+	this.history_back = [];
+	this.history_fwd = [];
 }
 extend(Infodex.prototype, {
+	history_back: null,
+	history_fwd: null,
 	
+	openPage: function(id) {
+		
+	},
 });
 module.exports = new Infodex();
 
