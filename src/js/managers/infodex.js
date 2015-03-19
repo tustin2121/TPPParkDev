a0ae4584b52a371a2aa07b3e47702bdef4182979
Tustin2121 <tustin2121@gmail.com>
2015-03-19 01:14:33 -0400

    Token work on Infodex

--- src/js/managers/infodex.js
+++ src/js/managers/infodex.js
@@ -2,3 +2,14 @@
 // The manager for the infodex. Other managers defer to this 
 // when calling functions for the infodex.
 
+var inherits = require("inherits");
+var extend = require("extend");
+
+function Infodex() {
+	
+}
+extend(Infodex.prototype, {
+	
+});
+module.exports = new Infodex();
+
